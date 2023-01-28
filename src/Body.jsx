ce3c7568e4e418daaa15fd518885358d0542372c
sakshamvedi <sakshamvedi@gmail.com>
2023-01-28 20:45:28 +0530

    main

---
+++ src/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Body.css";
+function Body() {
+  return (
+   <>
+   <div className="cover">
+   <div className="upper-bar"></div>
+<div className="mid-bar">
+<div className="box1">
+    <h1>
+        Hey !! my name is <b>Saksham</b>
+    </h1>
+    <p>I'm a Developer Passionate in making a beutiful Front-end and smooth UI/UX & partly love to code and learn new technology </p>
+</div>
+<div className="box2">
+</div>
+<div className="box2"></div>
+</div>
+</div>
+   </>
+  )
+}
+
+export default Body
